62613fbb684b05bf354f8f406f12b0388b7f6a84
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-10 18:01:38 +0200

    Initial Commit

---
+++ eatIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eatIT
+//
+//  Created by Denis Denisov on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
