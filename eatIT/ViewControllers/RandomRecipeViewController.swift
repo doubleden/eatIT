73ec54b04f62f11d98f690cadc6f3d241d3e93cf
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-12 16:18:38 +0200

    randomRecipe VC

--- eatIT/ViewControllers/RandomRecipeViewController.swift
+++ eatIT/ViewControllers/RandomRecipeViewController.swift
@@ -6,19 +6,43 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class RandomRecipeViewController: UIViewController {
 
     @IBOutlet var recipeImage: UIImageView!
-    @IBOutlet var summaryLabel: UIView!
+    @IBOutlet var summaryLabel: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    private var recipe: Recipe!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        recipeImage.layer.cornerRadius = 20
+        fetchData()
     }
 
-    @IBAction func rightBarButtonDidTapped(_ sender: UIBarButtonItem) {
+    @IBAction func updateButtonAction() {
+        fetchData()
     }
     
+    
+    @IBAction func saveRecipeAction() {
+    }
 }
 
+private extension RandomRecipeViewController {
+    func fetchData() {
+        networkManager.fetchRecipe(from: API.randomRecipe.url, with: API.randomRecipe.headers) { [unowned self] response in
+            switch response {
+            case .success(let recipes):
+                self.recipe = recipes.recipes.first
+                summaryLabel.text = recipe.summary
+                recipeImage.kf.setImage(with: URL(string: recipe.image))
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
